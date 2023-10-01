ba094d213ef75349c42904c4e59c4e0353e72609
KseniaLune <ksenia00nuysha@gmail.com>
2023-10-01 22:36:40 +0300

    #5 fix import

--- src/main/java/com/test_task/cars/Const.java
+++ src/main/java/com/test_task/cars/Const.java
@@ -1,9 +1,6 @@
 package com.test_task.cars;
 
-import java.time.LocalDateTime;
-
 public final class Const {
-
     public static final String regExpPattern = "[A-Z]\\d{3}[A-Z]{2}\\d{2,3}";
     public static final Integer minYear = 1850;
     public static final Integer maxYear = 2023;
